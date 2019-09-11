5f72c1235ef9875956766952e0f6c24d83de6a27
Carson Full <carson_full@tsco.org>
2019-09-10 22:51:54 -0500

    Create SplitClient which overrides the client in context

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { default as Split } from './Split';
+export * from './SplitClient';
 export { default as SplitProvider, SplitContext } from './SplitProvider';
 export * from './useSplit';
 export * from './useTrack';
